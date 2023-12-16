3b3e465292d9829f0db12345e788de8d34d43d05
ZakorAn <anya.zakoryukina@mail.ru>
2023-12-16 17:54:42 +0300

    Load_Hw8

---
+++ Assets/Scripts/CustomRenderPipeline.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace CustomRenderPipeline
+{
+    public class SpaceRunPipelineRenderAsset : RenderPipelineAsset
+    {
+        protected override RenderPipeline CreatePipeline()
+        {
+            return null;
+        }
+    }
+}
